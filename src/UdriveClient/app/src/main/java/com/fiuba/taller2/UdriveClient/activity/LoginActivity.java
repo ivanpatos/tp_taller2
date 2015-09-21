a60a531ea4f3ce7af6bab92832aa96ada17ca512
juliansci <julian.scialabba@gmail.com>
2015-09-21 01:10:53 -0300

    Agrego pantalla de login closes #3

--- src/UdriveClient/app/src/main/java/com/fiuba/taller2/UdriveClient/activity/LoginActivity.java
+++ src/UdriveClient/app/src/main/java/com/fiuba/taller2/UdriveClient/activity/LoginActivity.java
@@ -4,8 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fiuba.taller2.UdriveClient.R;
+import com.fiuba.taller2.UdriveClient.dto.RegisterDTO;
+import com.fiuba.taller2.UdriveClient.task.RegisterAsyncTask;
+import com.fiuba.taller2.UdriveClient.validator.RegisterValidator;
+import com.google.gson.Gson;
+
+import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,5 +45,9 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickLoginButton(View view) throws ExecutionException, InterruptedException {
+        Toast.makeText(this.getApplicationContext(), "Deberia loguear pero todavia no funciona", Toast.LENGTH_SHORT).show();
     }
 }
