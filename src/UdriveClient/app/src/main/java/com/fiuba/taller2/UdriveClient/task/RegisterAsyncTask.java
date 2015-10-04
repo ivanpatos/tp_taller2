f05e74e50ca10531554fdedc04e12a57552f63ce
juliansci <julian.scialabba@gmail.com>
2015-10-04 01:43:21 -0300

    Guardo datos del usuario registrado en shared preferences

--- src/UdriveClient/app/src/main/java/com/fiuba/taller2/UdriveClient/task/RegisterAsyncTask.java
+++ src/UdriveClient/app/src/main/java/com/fiuba/taller2/UdriveClient/task/RegisterAsyncTask.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.fiuba.taller2.UdriveClient.R;
 import com.fiuba.taller2.UdriveClient.activity.HomeActivity;
+import com.fiuba.taller2.UdriveClient.dto.UserDTO;
 import com.fiuba.taller2.UdriveClient.exception.ConnectionException;
 import com.fiuba.taller2.UdriveClient.util.PropertyManager;
+import com.google.gson.Gson;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.MalformedURLException;
@@ -61,6 +67,7 @@ public class RegisterAsyncTask extends AsyncTask<String, String, JSONObject> {
 
     @Override
     protected void onPostExecute(JSONObject jsonObject) {
+        Log.d("jsonobj",jsonObject.toString());
         dialog.dismiss();
         if(!errorMessage.isEmpty()){
             Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show();
@@ -84,6 +91,20 @@ public class RegisterAsyncTask extends AsyncTask<String, String, JSONObject> {
         } catch (Exception e) {
             e.printStackTrace();
         }
+      try {
+            Gson gson = new Gson();
+            UserDTO userDTO = gson.fromJson(jsonObject.get("data").toString(), UserDTO.class);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("name", userDTO.getName());
+            editor.putString("username", userDTO.getUsername());
+            editor.putString("mail", userDTO.getMail());
+            editor.putString("token", userDTO.getToken());
+            editor.apply();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         Intent intent = new Intent(activity, HomeActivity.class);
         activity.startActivity(intent);
         Toast.makeText(context, context.getString(R.string.register_success), Toast.LENGTH_SHORT).show();
