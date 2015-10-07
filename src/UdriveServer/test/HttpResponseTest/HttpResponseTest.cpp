24d420da582245a41e36d72e4b0617c70d406f6b
ltessore <ltessore@github.com>
2015-10-07 11:09:52 -0300

    Agrego test HttpResponseTest

---
+++ src/UdriveServer/test/HttpResponseTest/HttpResponseTest.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "../../main/include/server/HttpResponse.h"
+
+using ::testing::Return;
+using ::testing::_;
+
+const char* data = "data";
+
+std::string getResponse(const char* result){
+	Json::Value jsonResponse;
+	jsonResponse["result"] = result;
+	if (result == "ERROR"){
+		jsonResponse["errorCode"] = HttpResponse::ERROR_AUTHENTICATION;
+	}
+	else if (result == "OK"){
+    	jsonResponse["data"] = data;
+	}
+    Json::StreamWriterBuilder builder;
+	return Json::writeString(builder, jsonResponse);
+}
+TEST(HttpResponseTest, getHttpErrorResponse) {
+	HttpResponse httpResponse;
+	std::string response = httpResponse.GetHttpErrorResponse(HttpResponse::ERROR_AUTHENTICATION);
+    EXPECT_EQ(response, getResponse("ERROR"));
+}
+
+TEST(HttpResponseTest, getHttpErrorResponseInvalid) {
+	HttpResponse httpResponse;
+	std::string response = httpResponse.GetHttpErrorResponse(HttpResponse::ERROR_AUTHENTICATION);
+    EXPECT_TRUE(response != getResponse("OTHER"));
+}
+
+
+TEST(HttpResponseTest, getHttpOkResponse) {
+	HttpResponse httpResponse;
+	std::string response = httpResponse.GetHttpOkResponse(data);
+    EXPECT_EQ(response, getResponse("OK"));
+}
+
+TEST(HttpResponseTest, getHttpOkResponseInvalid) {
+	HttpResponse httpResponse;
+	std::string response = httpResponse.GetHttpOkResponse(data);
+    EXPECT_TRUE(response != getResponse("OTHER"));
+}
+
+
+int main(int argc, char *argv[]){
+	testing::InitGoogleTest(&argc,argv);
+	return RUN_ALL_TESTS();
+}
