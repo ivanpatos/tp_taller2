00ca5ca36f42e126019cdb5a3fcafa00e6f8b1c7
ltessore <ltessore@github.com>
2015-10-07 08:55:50 -0300

    Agrego test de ServiceFactory

---
+++ src/UdriveServer/test/ServiceFactoryTest.cpp
@@ -0,0 +1,117 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "../main/include/server/ServiceFactory.h"
+//#include "../main/include/server/Services/LoginService.h"
+//#include "../main/include/server/Services/LogoutService.h"
+
+using ::testing::Return;
+using ::testing::_;
+
+
+TEST(ServiceFactoryTest,loginService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string login = "login";
+	const std::string post = "POST";
+
+	std::string name = (serverFactory.createService(login, post))->name();
+	EXPECT_TRUE(name == "LoginService");
+}
+
+TEST(ServiceFactoryTest,logoutService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string logout = "logout";
+	const std::string post = "POST";
+
+	std::string name = (serverFactory.createService(logout, post))->name();
+	EXPECT_TRUE(name == "LogoutService");
+}
+
+TEST(ServiceFactoryTest,createFileService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "file";
+	const std::string method = "POST";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "CreateFileService");
+}
+TEST(ServiceFactoryTest,createFolderService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "folder";
+	const std::string method = "POST";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "CreateFolderService");
+}
+
+TEST(ServiceFactoryTest,createUserService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "user";
+	const std::string method = "POST";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "CreateUserService");
+}
+
+TEST(ServiceFactoryTest,getFileService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "file";
+	const std::string method = "GET";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "GetFileService");
+}
+TEST(ServiceFactoryTest,getFolderService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "folder";
+	const std::string method = "GET";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "GetFolderService");
+}
+TEST(ServiceFactoryTest,getUserService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "user";
+	const std::string method = "GET";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "GetUserService");
+}
+TEST(ServiceFactoryTest,nonExistentService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "other";
+	const std::string method = "other";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "NonExistentService");
+}
+TEST(ServiceFactoryTest,UpdateFileService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "file";
+	const std::string method = "PUT";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "UpdateFileService");
+}
+TEST(ServiceFactoryTest,updateUserService){
+	Database userDB("./userDB"), folderDB("./folderDB"), fileDB("./fileDB"), dataDB("./dataDB");
+	ServiceFactory serverFactory(userDB,folderDB,fileDB,dataDB);
+	const std::string resource = "user";
+	const std::string method = "PUT";
+
+	std::string name = (serverFactory.createService(resource, method))->name();
+	EXPECT_TRUE(name == "UpdateUserService");
+}
+int main(int argc, char *argv[]){
+	testing::InitGoogleTest(&argc,argv);
+	return RUN_ALL_TESTS();
+}
