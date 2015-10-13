c4f174495ac8b3b82a99ab3c02807b52fd543001
Artificiero <tomasreale@gmail.com>
2015-10-13 03:32:48 -0300

    deploy en Docker , closes #53

--- src/UdriveServer/main/code/Main.cpp
+++ src/UdriveServer/main/code/Main.cpp
@@ -1,8 +1,13 @@
 #include "../include/server/Server.h"
 
+#include "../include/utilities/easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
+
 int main() {
 
 	Server server;
+	LOG(INFO) << "Server UP and RUNNING";
 	server.start();
 
 	return 0;
